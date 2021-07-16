d428401ae41c9665499778be22d0862c4bba91ff
Douglas <douglasdat19@gmail.com>
2021-07-16 11:39:30 -0300

    3. Forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '9cdfdeda8dcb694055c2af3ea51176ce'
 
 the_posts = [
     {
@@ -27,6 +29,28 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About Page')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', category='success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # TODO TMP code
+        if form.email.data == 'a@b.com' and form.password.data == 'asdf':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -38,3 +38,30 @@ to overwrite the codeblock, simply call `{% block content %} {% endblock %}` and
 The endblock can also have the name of the block to indicate `{% endblock content %}`
 
 To link file to the html file, use `from flask import url_for`
+
+3. ###### Forms and User Input
+
+For security, is important to set a random key on the main file, with:
+`app.config['SECRET_KEY'] = '9cdfdeda8dcb694055c2af3ea51176ce'`
+
+to generate a random key, on the python console:
+`import secrets` and `secrets.token_hex(16)`, where 16 is the number of bytes
+
+`{{ form.hidden_tag() }}` on the html of the register and login for security reasons
+
+`from flask_wtf import FlaskForm` to work with forms.
+
+`from wtforms import StringField` to get the fields.
+
+`from wtforms.validators import DataRequired` are used to control data input settings
+
+To allow the page to POST or GET, change in the main file:
+`@app.route('/register', methods=['GET', 'POST'])`
+
+To check for a submit action use: `if form.validate_on_submit():`
+
+To display a message, use: `flash(f'Account created for {form.username.data}!', category='success')`
+
+To redirect: use `return redirect(url_for('home'))`
+
+
